5314e64527f85dccdfb9571d30871b79fba64799
Agustin Romero <agusromero0815@gmail.com>
2023-04-14 19:41:32 -0300

    creating cloudinary module and connection

---
+++ server/src/cloudinary/cloudinary.controller.ts
@@ -0,0 +1,39 @@
+import {
+  Controller,
+  HttpException,
+  HttpStatus,
+  Post,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
+import { CloudinaryService } from './cloudinary.service';
+import { Readable } from 'stream';
+
+@Controller('cloudinary')
+export class CloudinaryController {
+  constructor(private readonly cloudinaryService: CloudinaryService) {}
+
+  @Post('upload')
+  @UseInterceptors(FilesInterceptor('image'))
+  async uploadFile(@UploadedFiles() files): Promise<string[]> {
+    if (files.length > 3)
+      throw new HttpException(
+        'Limit exceeded, the maximum number of items is three',
+        HttpStatus.BAD_REQUEST,
+      );
+
+    const streams = files.map((file) => {
+      const stream = new Readable();
+      stream.push(file.buffer);
+      stream.push(null);
+
+      return stream;
+    });
+
+    const urls = await Promise.all(
+      streams.map((stream) => this.cloudinaryService.uploadImage(stream)),
+    );
+    return urls;
+  }
+}
