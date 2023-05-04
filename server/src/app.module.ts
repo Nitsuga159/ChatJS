ff62dc7047f5210f12c380ffa7de708d89ef85dd
Agustin Romero <agusromero0815@gmail.com>
2023-05-04 19:58:35 -0300

    update import http module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { DatabaseModule } from './database/database.module';
 import { CloudinaryModule } from './cloudinary/cloudinary.module';
-import { WsModule } from './ws/ws.module';
+import { HttpModule } from './http/http.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [DatabaseModule, CloudinaryModule, WsModule],
-  providers: [],
+  imports: [
+    MongooseModule.forRoot(
+      process.env.DB_HOST || 'mongodb://localhost:27017/chat_js',
+    ),
+    HttpModule,
+    CloudinaryModule,
+  ],
 })
 export class AppModule {}
--- server/src/main.ts
+++ server/src/main.ts
@@ -6,6 +6,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  await app.listen(3070);
+  await app.listen(process.env.PORT || 3070);
 }
 bootstrap();
